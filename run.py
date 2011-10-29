a1357a97b2612352e635edee1833de0854232c48
Ben M <bmac2558@uni.sydney.edu.au>
2011-10-29 16:38:43 +1100

    Add an example Jump file.
    Also allow using stdin with run.py.

--- run.py
+++ run.py
@@ -4,17 +4,22 @@ import antlr3
 from build.JumpLexer import JumpLexer
 from build.JumpParser import JumpParser
 
-def main(filename):
-    char_stream = antlr3.ANTLRFileStream(filename)
+def main(fileobj):
+    char_stream = antlr3.ANTLRInputStream(fileobj)
     tokens = antlr3.CommonTokenStream(JumpLexer(char_stream))
     parser = JumpParser(tokens)
-    graph = parser.prog().graph
+    root = parser.prog()
 
-    graph.print_source(sys.stdout)
+    print root.tree.toStringTree()
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print >> sys.stderr, 'Usage: {0} <filename>'.format(sys.argv[0])
+    if len(sys.argv) > 2 or (len(sys.argv) == 1 and sys.argv[1] in ('-h', '--help')):
+        print >> sys.stderr, 'Usage: {0} [filename]'.format(sys.argv[0])
         sys.exit(1)
-    main(sys.argv[1])
+
+    if len(sys.argv) == 2:
+        with open(sys.argv[1]) as f:
+            main(f)
+    else:
+        main(sys.stdin)
